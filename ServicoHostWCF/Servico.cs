e5a8fd40b65095ef2e93abb0f46fed79f9197b61
logatti <logatti@fiar.local>
2019-10-08 21:09:03 -0300

    first commit

---
+++ ServicoHostWCF/Servico.cs
@@ -0,0 +1,25 @@
+﻿using Entidade;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServicoHostWCF
+{
+    // OBSERVAÇÃO: Você pode usar o comando "Renomear" no menu "Refatorar" para alterar o nome da classe "Servico" no arquivo de código e configuração ao mesmo tempo.
+    public class Servico : IServico
+    {
+        TesteCadastroBO boTesteCadastro;
+        
+        public List <EntTesteCadastro> Consultar (String nome)
+        {
+            boTesteCadastro = new TesteCadastroBO();
+            EntTesteCadastro cliente = new EntTesteCadastro();
+            cliente.Nome = nome;
+            return boTesteCadastro.BuscarCliente(cliente);
+        }
+    }
+}
